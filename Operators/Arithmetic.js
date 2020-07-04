fa35c14b54b7a4d7e5e7d84a72f67eb19fc20901
surajitsarkarofficial <surajitsarkarofficial@gmail.com>
2020-07-04 09:39:34 +0530

    Added Arthimetic Operator code

---
+++ Operators/Arithmetic.js
@@ -0,0 +1,7 @@
+var num1 = 10;
+var num2 = 5;
+alert("Addition of " + num1 + " and " + num2 + " is = " + (num1 + num2));
+alert("Subrtraction of " + num1 + " and " + num2 + " is = " + (num1 - num2));
+alert("Multiplication of " + num1 + " and " + num2 + " is = " + (num1 * num2));
+alert("Division of " + num1 + " and " + num2 + " is = " + (num1 / num2));
+alert("Mod of " + num1 + " and " + num2 + " is = " + (num1 % num2));
---
+++ Operators/Arithmetic.ts
@@ -0,0 +1,8 @@
+var num1:number = 10;
+var num2:number = 5;
+
+alert("Addition of "+num1+" and "+num2 +" is = "+(num1+num2));
+alert("Subrtraction of "+num1+" and "+num2 +" is = "+(num1-num2));
+alert("Multiplication of "+num1+" and "+num2 +" is = "+(num1*num2));
+alert("Division of "+num1+" and "+num2 +" is = "+(num1/num2));
+alert("Mod of "+num1+" and "+num2 +" is = "+(num1%num2));
