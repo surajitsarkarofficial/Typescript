847a36fdd8e3adf38e676e1baa5cdedb7c7baef1
surajitsarkarofficial <surajitsarkarofficial@gmail.com>
2020-07-04 09:29:24 +0530

    Added Pop-ups code

---
+++ Popups/Popups.js
@@ -0,0 +1,7 @@
+//alert
+alert("This is an Alert Pop-up.");
+//Confirm
+confirm("This is a Confirm Pop-up. Do you wish to continue ?");
+//Prompt
+var promptName = prompt("This is a Prompt Pop-up. Please Enter your Name.");
+alert("Thank you " + promptName + ".");
---
+++ Popups/Popups.ts
@@ -0,0 +1,9 @@
+//alert
+alert("This is an Alert Pop-up.");
+
+//Confirm
+confirm("This is a Confirm Pop-up. Do you wish to continue ?");
+
+//Prompt
+var promptName:String =prompt("This is a Prompt Pop-up. Please Enter your Name.");
+alert("Thank you "+ promptName+".");
